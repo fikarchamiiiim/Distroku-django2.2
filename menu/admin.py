78fcbe916139a13ca444ba795dc93276cfe06e62
fikarchamiiiim <fikarchamim@yahoo.com>
2019-10-29 11:10:26 +0700

    first commit

---
+++ menu/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Menu,UserProfileInfo, Variation
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('judulMenu','picMenu','hargaMenu','kuantitas','slug','timestamp','updated','active','JK')
+    date_hierarchy = ('updated')
+    search_fields = ['judulMenu']
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user','alamat','Provinsi','Kota','Kecamatan','Kelurahan','profile_pic')
+    search_fields = ['user']
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product','category','title','updated','active')
+    search_fields = ['product']
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(UserProfileInfo, UserAdmin)
+admin.site.register(Variation, VariationAdmin)
